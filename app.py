d6c8dcfe4e3a7bfcde4393064a7e34ca56235fbd
Patrick Garrett <86084767+pgarrett-scripps@users.noreply.github.com>
2025-05-29 21:37:05 +0000

    protein_id

--- app.py
+++ app.py
@@ -3,8 +3,20 @@ from src.fastaframes.fastaframes import to_df
 
 fasta = st.file_uploader("Upload FASTA file", type=".fasta")
 
+
+def get_protein_id(row):
+
+    elems = [row['db'], row['unique_identifier'], row['entry_name']]
+    elems = [e for e in elems if e is not None and e != 'None']
+    return "|".join(elems)
+
 if fasta:
     df = to_df(fasta)
 
+    # 
+    df['protein_id'] = df.apply(get_protein_id, axis=1)
+
     # display the dataframe
     st.dataframe(df)
+
+    
